cf4dd8454df67d76e115499b5daa415e2ca9e5e8
taidoan2k <54546970+taidoan2k@users.noreply.github.com>
2022-11-14 16:10:16 -0500

    create aggie coin
    a

--- context/constant.js
+++ context/constant.js
@@ -5,8 +5,8 @@
 import funToken from "./FunToken.json";
 import funTokenSale from "./FunTokenSale.json";
 
-export const funTokenAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const funTokenAddress = "0xaa8e3071D18BBC01C7DDa8aA19B82125E446883E";
 export const funTokenABI = funToken.abi;
 
-export const funTokenSaleAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const funTokenSaleAddress = "0x7a863ceA407ee6C8015E79F65E2666541BBfa3Da";
 export const funTokenSaleABI = funTokenSale.abi;
